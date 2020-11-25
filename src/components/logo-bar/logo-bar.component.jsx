0755a72d80594197577fd85f32be308d1dfbcd42
Skynet966 <satishkumaryadav72@gmail.com>
2020-11-25 20:51:28 +0530

    initilize the app with base settings and directories

---
+++ src/components/logo-bar/logo-bar.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './logo-bar.styles.sass';
+
+const LogoBar = () => <h1>This is logo bar</h1>;
+
+export default LogoBar;
---
+++ src/components/search-bar/search-bar.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './search-bar.styles.sass';
+
+const SearchBar = () => <p>This is nav bar</p>;
+
+export default SearchBar;
