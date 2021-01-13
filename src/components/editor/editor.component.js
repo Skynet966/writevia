f04e49df4bc08c3beeff7cdbb6e01c4aa3b9d3fd
Skynet966 <satishkumaryadav72@gmail.com>
2021-01-13 09:54:00 +0530

    editor with basic functionality done

---
+++ src/components/editor/editor.component.js
@@ -0,0 +1,158 @@
+import React, { useEffect } from 'react';
+//Editor JS WITH TOOLS
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+// import Paragraph from '@editorjs/paragraph';
+import Paragraph from 'editorjs-paragraph-with-alignment';
+import Quote from '@editorjs/quote';
+import Warning from '@editorjs/warning';
+import Delimiter from '@editorjs/delimiter';
+import Alert from 'editorjs-alert';
+import List from '@editorjs/list';
+import ImageTool from '@editorjs/image';
+import LinkTool from '@editorjs/link';
+import AttachesTool from '@editorjs/attaches';
+import Embed from '@editorjs/embed';
+import SocialPost from 'editorjs-social-post-plugin';
+import Table from 'editorjs-table';
+import CodeTool from '@editorjs/code';
+import Marker from '@editorjs/marker';
+import InlineCode from '@editorjs/inline-code';
+import Underline from '@editorjs/underline';
+//Uninstall these non useable items
+// import Button from 'editorjs-button';
+// import Table from '@editorjs/table';
+// import InlineImage from 'editorjs-inline-image';
+// import SimpleImage from 'simple-image-editorjs';
+// import HeaderAnchor from 'editorjs-header-with-anchor';
+
+import styled from 'styled-components';
+
+export const EditorContainer = styled.div`
+	border: solid 2px black;
+	padding: 30px 20px;
+	text-align: -webkit-auto;
+`;
+
+const Editor = () => {
+	useEffect(() => {
+		new EditorJS({
+			placeholder: 'Write you post body here',
+			holder: 'editorjs',
+			tools: {
+				header: {
+					class: Header,
+					shortcut: 'CMD+SHIFT+H',
+					config: {
+						placeholder: 'Enter a header',
+						levels: [2, 3, 4, 5, 6],
+						defaultLevel: 4
+					}
+				},
+				quote: {
+					class: Quote,
+					inlineToolbar: true,
+					shortcut: 'CMD+SHIFT+O',
+					config: {
+						quotePlaceholder: 'Enter a quote',
+						captionPlaceholder: "Quote's author"
+					}
+				},
+				linkTool: {
+					class: LinkTool,
+					config: {
+						endpoint: `${
+							!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+								? 'http://localhost:3000'
+								: 'https://writevia-backend.herokuapp.com'
+						}/utils/fetchUrl` // Your backend endpoint for url data fetching
+					}
+				},
+				embed: {
+					class: Embed,
+					config: {
+						services: {
+							youtube: true
+						}
+					}
+				},
+				warning: {
+					class: Warning,
+					inlineToolbar: true,
+					shortcut: 'CMD+SHIFT+W',
+					config: {
+						titlePlaceholder: 'Title',
+						messagePlaceholder: 'Message'
+					}
+				},
+				alert: {
+					class: Alert,
+					inlineToolbar: true,
+					shortcut: 'CMD+SHIFT+A',
+					config: {
+						defaultType: 'primary',
+						messagePlaceholder: 'Enter alert Message...'
+					}
+				},
+				Marker: {
+					class: Marker,
+					shortcut: 'CMD+SHIFT+M'
+				},
+				inlineCode: {
+					class: InlineCode,
+					shortcut: 'CMD+SHIFT+C'
+				},
+				underline: {
+					class: Underline,
+					shortcut: 'CMD+SHIFT+U'
+				},
+				paragraph: {
+					class: Paragraph,
+					inlineToolbar: true
+				},
+				image: {
+					class: ImageTool,
+					config: {
+						endpoints: {
+							byFile: `${
+								!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+									? 'http://localhost:3000'
+									: 'https://writevia-backend.herokuapp.com'
+							}/upload/image`, // Your backend file uploader endpoint
+							byUrl: `${
+								!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+									? 'http://localhost:3000'
+									: 'https://writevia-backend.herokuapp.com'
+							}/upload/urlImage` // Your endpoint that provides uploading by Url
+						}
+					}
+				},
+				attaches: {
+					class: AttachesTool,
+					config: {
+						endpoint: `${
+							!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+								? 'http://localhost:3000'
+								: 'https://writevia-backend.herokuapp.com'
+						}/upload/file`
+					}
+				},
+				table: {
+					class: Table,
+					inlineToolbar: true,
+					config: {
+						rows: 2,
+						cols: 3
+					}
+				},
+				socialPost: SocialPost,
+				code: CodeTool,
+				delimiter: Delimiter,
+				list: List
+			}
+		});
+	}, []);
+	return <EditorContainer id='editorjs'></EditorContainer>;
+};
+
+export default Editor;
--- src/pages/create-post-page/create-post-page.page.jsx
+++ src/pages/create-post-page/create-post-page.page.jsx
@@ -1,88 +1,97 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import styled from 'styled-components';
+import Editor from '../../components/editor/editor.component';
 import FormInput from '../../components/form-input/form-input.component';
+import { categoriesFetching } from '../../redux/categories/categories.action';
+import { selectCategoriesList } from '../../redux/categories/categories.selectors';
 
 const CreatePostPageContainer = styled.div``;
 
-const CreatePostPage = () => (
-	<CreatePostPageContainer>
-		<div className='container'>
-			<div className='row py-5'>
-				<div className='col-12'>
-					<form className='row'>
-						<div className='col-md-12'>
-							<FormInput type='text' placeholder='Post title' required />
-						</div>
-						<div className='col-md-12'>
-							<FormInput
-								type='textarea'
-								placeholder='Post Shot Desc'
-								required
-							/>
-						</div>
-						<div className='col-md-6'>
-							<FormInput type='select' required>
-								<option value='' selected disabled>
-									{' '}
-									-- Select the category --{' '}
-								</option>
-								<option value='Technology'>Technology</option>
-								<option value='News'>News</option>
-								<option value='Entertainment'>Entertainment</option>
-								<option value='Sports'>Sports</option>
-								<option value='Politics'>Politics</option>
-								<option value='Science'>Science</option>
-								<option value='Movies'>Movies</option>
-								<option value='Education'>Education</option>
-								<option value='Career'>Career</option>
-								<option value='Trending'>Trending</option>
-								<option value='Lifestyle'>Lifestyle</option>
-								<option value='Food'>Food</option>
-								<option value='Travel'>Travel</option>
-								<option value='Gaming'>Gaming</option>
-								<option value='Media'>Media</option>
-								<option value='Fashion'>Fashion</option>
-								<option value='Health'>Health</option>
-								<option value='Finance'>Finance</option>
-								<option value='Economy'>Economy</option>
-							</FormInput>
-						</div>
-						<div className='col-md-6'>
-							<div className='row'>
-								<div className='col-md-6'>
-									<FormInput type='url' placeholder='Post Image Source url' />
-								</div>
-								<div className='col-md-6'>
-									<FormInput type='text' placeholder='Post Image Source Name' />
+const CreatePostPage = ({ categories, fetchCategories }) => {
+	useEffect(() => {
+		fetchCategories();
+	}, [fetchCategories]);
+
+	return (
+		<CreatePostPageContainer>
+			<div className='container'>
+				<div className='row py-5'>
+					<div className='col-12'>
+						<form className='row'>
+							<div className='col-md-12'>
+								<FormInput
+									type='text'
+									name='title'
+									placeholder='Post title'
+									required
+								/>
+							</div>
+							<div className='col-md-12'>
+								<FormInput
+									type='textarea'
+									name='desc'
+									placeholder='Post Shot Desc'
+									required
+								/>
+							</div>
+							<div className='col-md-6'>
+								<FormInput name='category' type='select' required>
+									<option value='' disabled>
+										{' '}
+										-- Select the category --{' '}
+									</option>
+									{categories.map(category => (
+										<option key={category.created_at} value={category.title}>
+											{category.title}
+										</option>
+									))}
+								</FormInput>
+							</div>
+							<div className='col-md-6'>
+								<div className='row'>
+									<div className='col-md-6'>
+										<FormInput
+											name='imageUrl'
+											type='url'
+											placeholder='Post Image Source url'
+										/>
+									</div>
+									<div className='col-md-6'>
+										<FormInput
+											name='imageSource'
+											type='text'
+											placeholder='Post Image Source Name'
+										/>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div className='col-md-6'>
-							<FormInput type='range' min='1' max='20' />
-						</div>
-						<div className='col-md-6'>
-							<FormInput type='text' placeholder='post tags' />
-						</div>
-						<div className='col-md-6'>
-							<FormInput type='select' required>
-								<option defaultValue='' defaultSelected disabled>
-									{' '}
-									-- select Author --{' '}
-								</option>
-								<option value='Rahul Maulick'>Rahul Maulick</option>
-								<option value='Amanpreet Kaur'>Amanpreet Kaur</option>
-								<option value='Keertanya S.'>Keertanya S.</option>
-							</FormInput>
-						</div>
-						<div className='col-md-6'></div>
-						<div className='col-md-6'></div>
-						<div className='col-md-6'></div>
-					</form>
+							<div className='col-md-6'>
+								<FormInput name='readTime' type='range' min='1' max='20' />
+							</div>
+							<div className='col-md-6'>
+								<FormInput name='tags' type='text' placeholder='post tags' />
+							</div>
+							<div className='col-md-12'>
+								<Editor />
+							</div>
+							<div className='col-md-6'></div>
+							<div className='col-md-6'></div>
+						</form>
+					</div>
 				</div>
 			</div>
-		</div>
-	</CreatePostPageContainer>
-);
+		</CreatePostPageContainer>
+	);
+};
+const mapStateToProps = createStructuredSelector({
+	categories: selectCategoriesList
+});
 
-export default CreatePostPage;
+const mapDispatchToProps = dispatch => ({
+	fetchCategories: () => dispatch(categoriesFetching())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreatePostPage);
